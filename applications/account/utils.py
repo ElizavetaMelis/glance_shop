6dbcb8dd6599829b0970fa8804bd6df453f3874f
ElizavetaMelis <elizavetamelisova@gmail.com>
2022-03-08 18:36:41 +0600

    project skeleton, account

---
+++ applications/account/utils.py
@@ -0,0 +1,18 @@
+from django.core.mail import send_mail
+
+
+def send_activation_email(email, activation_code):
+    activation_url = f'http://localhost:8000/account/activate/{activation_code}/'
+    message = f'''
+                    Thank you for signing up.
+                    Please, activate your account.
+                    Activation link: {activation_url}'''
+
+    send_mail(
+        'Activate your account',
+        message,
+        'store@glance.kg',
+        [email, ],
+        # show error
+        fail_silently=False,
+    )
